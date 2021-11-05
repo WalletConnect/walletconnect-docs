c1573ef7ab3be5f2d97a09b625d835769df9a914
Chad <chadyj@users.noreply.github.com>
2021-11-05 09:49:55 +0100

    add favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  //favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.ico',
   organizationName: 'walletconnect', // Usually your GitHub org/user name.
   projectName: 'walletconnect-docs', // Usually your repo name.
 
