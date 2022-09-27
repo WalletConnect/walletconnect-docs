a5ec6542fd648d21401b748480b178e209c22dcb
Chad Jackson <chadyj@users.noreply.github.com>
2022-09-27 11:13:21 +0200

    remove duplicate relay/projectid page

--- sidebars.js
+++ sidebars.js
@@ -26,7 +26,7 @@ module.exports = {
           type: "category",
           label: "Our Projects",
           collapsed: false,
-          items: ["introduction/cloud", "introduction/cloud-explorer"],
+          items: ["introduction/cloud-explorer"],
         },
         "introduction/examples-and-resources",
         "introduction/glossary",
