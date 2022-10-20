45ac62359bbea830435851bd6f55509a36206fe0
Chad Jackson <chadyj@users.noreply.github.com>
2022-10-21 00:35:11 +0200

    adds web3modal page

--- sidebars.js
+++ sidebars.js
@@ -26,7 +26,10 @@ module.exports = {
           type: "category",
           label: "Our Projects",
           collapsed: false,
-          items: ["introduction/cloud-explorer"],
+          items: [
+            "introduction/cloud-explorer",
+            "introduction/web3modal"
+          ],
         },
         "introduction/examples-and-resources",
         "introduction/glossary",
