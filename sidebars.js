291a7c0e38cefe5fef43eab97a1721dfda7be557
Vanessa Mercado <finessevanes@Vanessas-MacBook-Pro.local>
2022-10-21 13:15:52 -0500

    docs: rename dapp usage page

--- sidebars.js
+++ sidebars.js
@@ -48,7 +48,7 @@ module.exports = {
           items: [
             "javascript/sign/installation",
             "javascript/sign/wallet-usage",
-            "javascript/sign/dapp-usage-standalone-client",
+            "javascript/sign/dapp-usage",
           ],
         },
         {
