028ecfdcd664e3b5bd660f50a3c0d35f634aaed1
Vanessa Mercado <finessevanes@Vanessas-MacBook-Pro.local>
2022-11-12 17:08:56 -0700

    move styles in css file

--- docs/components/Container.js
+++ docs/components/Container.js
@@ -1,76 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import Box from './Box'
 
 export const Container = () => {
-  const [isMobile, setIsMobile] = useState(false);
-  let viewportWidth = window.innerWidth;
-
-useEffect(() => {
-if(viewportWidth <= 703){
-    setIsMobile(true)
-}
-
-}, viewportWidth)
-
-  const BoxStyle = {
-    width: "19rem",
-    border: "1px solid black",
-    borderRadius: "7px",
-    backgroundColor: "#242124",
-    boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;",
-    paddingInline: "1em",
-    paddingTop: "1em",
-    marginBottom: "1em",
-    marginRight: "1em",
-  };
-
-  const TitleStyle = {
-    fontSize: "20px",
-  };
-
   return (
     <div className="container">
-      <div style={BoxStyle}>
-        <h1 style={TitleStyle}>Auth</h1>
-        <p>
-          An authentication protocol that can be used to log-in blockchain
-          wallets into apps.
-        </p>
-      </div>
-      <div style={BoxStyle}>
-        <h1 style={TitleStyle}>Auth</h1>
-        <p>
-          An authentication protocol that can be used to log-in blockchain
-          wallets into apps.
-        </p>
-      </div>
-      <div style={BoxStyle}>
-        <h1 style={TitleStyle}>Auth</h1>
-        <p>
-          An authentication protocol that can be used to log-in blockchain
-          wallets into apps.
-        </p>
-      </div>
-      <div style={BoxStyle}>
-        <h1 style={TitleStyle}>Auth</h1>
-        <p>
-          An authentication protocol that can be used to log-in blockchain
-          wallets into apps.
-        </p>
-      </div>
-      <div style={BoxStyle}>
-        <h1 style={TitleStyle}>Auth</h1>
-        <p>
-          An authentication protocol that can be used to log-in blockchain
-          wallets into apps.
-        </p>
-      </div>
-      <div style={BoxStyle}>
-        <h1 style={TitleStyle}>Auth</h1>
-        <p>
-          An authentication protocol that can be used to log-in blockchain
-          wallets into apps.
-        </p>
-      </div>
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
     </div>
   );
 };
