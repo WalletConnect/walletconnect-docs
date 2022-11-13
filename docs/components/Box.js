87e4018b0bf57552a97e29afc7900109d21bb6bf
Vanessa Mercado <finessevanes@Vanessas-MacBook-Pro.local>
2022-11-13 06:51:31 -0700

    routing works

--- docs/components/Box.js
+++ docs/components/Box.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-const Box = ({ name, description}) => {
+const Box = ({ name, description, url}) => {
+  let history = useHistory();
+  const handleClick = () => {
+    history.push(`/2.0/introduction/${url}`)
+  }
   return (
-    <div className="box">
+    <div className="box" onClick={handleClick}>
       <h1 style={{ fontSize: "20px" }}>{name}</h1>
       <p>{description}</p>
     </div>
--- docs/components/Container.js
+++ docs/components/Container.js
@@ -4,19 +4,23 @@ import Box from './Box'
 const CONSTANTS = [
   {
     name: "Sign",
-    description: "Sign is a remote signer protocol to communicate securely between web3 wallets and dapps."
+    description: "Sign is a remote signer protocol to communicate securely between web3 wallets and dapps.",
+    url: "sign"
   },
   {
     name: "Auth",
-    description: "Auth is an authentication protocol that can be used to log-in blockchain wallets into apps."
+    description: "Auth is an authentication protocol that can be used to log-in blockchain wallets into apps.",
+    url: "auth"
   },
   {
     name: "Push",
-    description: "Chat is a direct messaging protocol that can enable wallets to message each other."
+    description: "Chat is a direct messaging protocol that can enable wallets to message each other.",
+    url: "push"
   },
   {
     name: "Chat",
-    description: "Push is a push notification protocol that enables apps to notify users of both off-chain and on-chain events."
+    description: "Push is a push notification protocol that enables apps to notify users of both off-chain and on-chain events.",
+    url: "chat"
   }
 ]
 
@@ -24,7 +28,7 @@ export const Container = () => {
   return (
     <div className="container">
       {CONSTANTS.map((i)=>(
-        <Box name={i.name} description={i.description} />
+        <Box name={i.name} description={i.description} url={url}/>
       ))}
     </div>
   );
