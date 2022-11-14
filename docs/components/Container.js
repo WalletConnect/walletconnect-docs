c57166c4856e47787cb081379f7186e677400dac
Vanessa Mercado <finessevanes@Vanessas-MacBook-Pro.local>
2022-11-14 09:55:56 -0700

    PR feedback /2

--- docs/components/Container.js
+++ docs/components/Container.js
@@ -1,29 +1,29 @@
 import React from "react";
 import Box from './Box'
 
+const URL_SCHEME = "/2.0/introduction/";
 const CONSTANTS = [
   {
     name: "Sign",
     description: "Sign is a remote signer protocol to communicate securely between web3 wallets and dapps.",
-    url: "sign"
+    url: `${URL_SCHEME}sign`
   },
   {
     name: "Auth",
     description: "Auth is an authentication protocol that can be used to log-in blockchain wallets into apps.",
-    url: "auth"
+    url: `${URL_SCHEME}auth`
   },
   {
     name: "Push",
     description: "Push is a push notification protocol that enables apps to notify users of both off-chain and on-chain events.",
-    url: "push"
+    url: `${URL_SCHEME}push`
   },
   {
     name: "Chat",
     description: "Chat is a direct messaging protocol that can enable wallets to message each other.",
-  
-    url: "chat"
+    url: `${URL_SCHEME}chat`
   }
-]
+];
 
 export const Container = () => {
   return (
