315e845b3a798cdbf8bb5f25b0a27709c9be78cb
Pedro Gomes <pedrogomes94@gmail.com>
2023-05-02 15:31:34 +0200

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -244,7 +244,7 @@ module.exports = {
     {
       type: 'html',
       value:
-        '<a class="navbar__brand sidebar__brand" href="/"><div class="navbar__logo"><img src="/img/walletconnect-logo-white.svg" alt="WalletConnect Logo"></div>WalletConnect<span>Docs<span></a>',
+        '<a class="navbar__brand" href="/"><div class="navbar__logo"><img src="/img/walletconnect-logo-white.svg#dark-mode-only" alt="WalletConnect Logo"><img src="/img/walletconnect-logo-black.svg#light-mode-only" alt="WalletConnect Logo"></div>WalletConnect<span>Docs<span></a>'
       defaultStyle: true
     },
     {
