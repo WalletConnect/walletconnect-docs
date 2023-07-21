55951c6d7ac2d414133c8f03129a05f7c877a12d
Boidushya Bhattacharya <boidushyabhattacharya@gmail.com>
2023-07-21 21:03:24 +0530

    feat: added more content for web3wallet

--- docs/components/CloudBanner.js
+++ docs/components/CloudBanner.js
@@ -9,7 +9,7 @@ export const CloudBanner = props => {
         <p>Head over to WalletConnect Cloud and create a New Project now!</p>
       </div>
       <a href="https://cloud.walletconnect.com" target="_blank">
-        Get started for free
+        Get started
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
