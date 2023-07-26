f5c32bb3d969ce307827723d6f18327bde5fa76e
Boidushya Bhattacharya <boidushyabhattacharya@gmail.com>
2023-07-26 23:13:03 +0530

    feat: feedback css

--- docs/components/Feedback.js
+++ docs/components/Feedback.js
@@ -1,4 +1,5 @@
 import React, { useCallback, useState } from 'react'
+import styles from '../../src/css/feedback.module.css'
 
 const submitFeedback = async ({ feedback, path, text }) => {
   const props = {
@@ -23,10 +24,12 @@ const DetailedFeedback = ({ feedback, reset, path }) => {
     reset()
   }
   return (
-    <div className="feedback__detailed">
+    <div className={styles.feedback__detailed}>
       {feedback === 1 || submitted ? (
         <>
-          <p className="feedback__detailed__text feedback__detailed__text--positive">
+          <p
+            className={`${styles.feedback__detailed__text} ${styles['feedback__detailed__text--positive']}`}
+          >
             Thanks for the feedback!
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
               <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
@@ -35,9 +38,9 @@ const DetailedFeedback = ({ feedback, reset, path }) => {
         </>
       ) : (
         <>
-          <p className="feedback__detailed__text">What went wrong?</p>
+          <p className={styles.feedback__detailed__text}>What went wrong?</p>
           {otherClicked ? (
-            <div className="feedback__detailed__other">
+            <div className={styles.feedback__detailed__other}>
               <textarea
                 placeholder="Describe the issue you encountered"
                 value={text}
@@ -49,28 +52,28 @@ const DetailedFeedback = ({ feedback, reset, path }) => {
             </div>
           ) : (
             <>
-              <div className="feedback__detailed__options">
+              <div className={styles.feedback__detailed__options}>
                 <button
-                  className="feedback__detailed__option"
+                  className={styles.feedback__detailed__option}
                   onClick={() => handleSubmit('incorrect')}
                 >
                   Information was incorrect{' '}
                 </button>
                 <button
-                  className="feedback__detailed__option"
+                  className={styles.feedback__detailed__option}
                   onClick={() => handleSubmit('outdated')}
                 >
                   Information was outdated
                 </button>
                 <button
-                  className="feedback__detailed__option"
+                  className={styles.feedback__detailed__option}
                   onClick={() => handleSubmit('unclear')}
                 >
                   Information was unclear
                 </button>
                 <button
                   onClick={() => setOtherClicked(true)}
-                  className="feedback__detailed__option feedback__detailed__option--other"
+                  className={`${styles.feedback__detailed__option} ${styles['feedback__detailed__option--other']}`}
                 >
                   Something Else
                 </button>
@@ -103,16 +106,16 @@ const Feedback = ({ path }) => {
     }
   }
   return (
-    <div className="feedback">
+    <div className={`feedback ${styles.feedback}`}>
       {feedback ? (
         <DetailedFeedback feedback={feedback} reset={reset} path={path} />
       ) : (
         <>
-          <p className="feedback__text">Was this helpful?</p>
-          <div className="feedback__options">
+          <p className={styles.feedback__text}>Was this helpful?</p>
+          <div className={styles.feedback__options}>
             <button
               onClick={() => handleClick(1)}
-              className="feedback__option feedback__option--yes"
+              className={`${styles.feedback__option} ${styles['feedback__option--yes']}`}
             >
               Yes
               <svg
@@ -131,7 +134,7 @@ const Feedback = ({ path }) => {
             </button>
             <button
               onClick={() => handleClick(-1)}
-              className="feedback__option feedback__option--no"
+              className={`${styles.feedback__option} ${styles['feedback__option--no']}`}
             >
               No
               <svg
