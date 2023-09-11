6644353acfd375cd2ffb2f8c3d40eaf7e1e07eac
Glitch <glitch.txs@gmail.com>
2023-09-11 12:19:42 -0300

    typo

--- sidebars.js
+++ sidebars.js
@@ -354,9 +354,9 @@ module.exports = {
           label: 'React',
           items: [
             { type: 'doc', label: 'Getting started', id: 'web3modal/v3/react/about' },
-            { type: 'doc', label: 'Options', id: 'web3modal/v3/react/options' },
-            { type: 'doc', label: 'Components', id: 'web3modal/v3/react/components' },
-            { type: 'doc', label: 'Theming', id: 'web3modal/v3/react/theming' },
+            'web3modal/v3/react/options',
+            'web3modal/v3/react/components',
+            'web3modal/v3/react/theming',
             'web3modal/v3/react/hooks',
           ]
         },
@@ -366,10 +366,9 @@ module.exports = {
           label: 'Vue',
           items: [
             { type: 'doc', label: 'Getting started', id: 'web3modal/v3/vue/about' },
-            { type: 'doc', label: 'Options', id: 'web3modal/v3/vue/options' },
-            { type: 'doc', label: 'Components', id: 'web3modal/v3/vue/components' },
-            { type: 'doc', label: 'Theming', id: 'web3modal/v3/vue/theming' },
-            ,
+            'web3modal/v3/vue/options',
+            'web3modal/v3/vue/components',
+            'web3modal/v3/vue/theming',
             'web3modal/v3/vue/composables',
           ]
         },
@@ -379,9 +378,9 @@ module.exports = {
           label: 'HTML',
           items: [
             { type: 'doc', label: 'Getting started', id: 'web3modal/v3/html/about' },
-            { type: 'doc', label: 'Options', id: 'web3modal/v3/html/options' },
-            { type: 'doc', label: 'Components', id: 'web3modal/v3/html/components' },
-            { type: 'doc', label: 'Theming', id: 'web3modal/v3/html/theming' },
+            'web3modal/v3/html/options',
+            'web3modal/v3/html/components',
+            'web3modal/v3/html/theming',
             'web3modal/v3/html/actions',
           ]
         },
