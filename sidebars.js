b43a68358b0c67a26d3ed9564941900d8850d9f3
Glitch <glitch.txs@gmail.com>
2023-09-13 11:27:24 -0300

    add wagmi hooks

--- sidebars.js
+++ sidebars.js
@@ -354,10 +354,10 @@ module.exports = {
           label: 'React',
           items: [
             { type: 'doc', label: 'Getting started', id: 'web3modal/v3/react/about' },
+            'web3modal/v3/react/hooks',
             'web3modal/v3/react/options',
             'web3modal/v3/react/components',
             'web3modal/v3/react/theming',
-            'web3modal/v3/react/hooks',
           ]
         },
         {
@@ -366,10 +366,10 @@ module.exports = {
           label: 'Vue',
           items: [
             { type: 'doc', label: 'Getting started', id: 'web3modal/v3/vue/about' },
+            'web3modal/v3/vue/composables',
             'web3modal/v3/vue/options',
             'web3modal/v3/vue/components',
             'web3modal/v3/vue/theming',
-            'web3modal/v3/vue/composables',
           ]
         },
         {
@@ -378,10 +378,10 @@ module.exports = {
           label: 'HTML',
           items: [
             { type: 'doc', label: 'Getting started', id: 'web3modal/v3/html/about' },
+            'web3modal/v3/html/actions',
             'web3modal/v3/html/options',
             'web3modal/v3/html/components',
             'web3modal/v3/html/theming',
-            'web3modal/v3/html/actions',
           ]
         },
         'web3modal/v3/upgrade',
