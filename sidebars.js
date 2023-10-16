bf282bef410fb09d01a016dcf8cf1a3a03d89dcc
Boidushya Bhattacharya <boidushyabhattacharya@gmail.com>
2023-10-16 16:38:16 +0530

    fix: rn section

--- sidebars.js
+++ sidebars.js
@@ -68,6 +68,18 @@ module.exports = {
             },
             {
               type: 'category',
+              className: 'sidebar__logo sidebar__logo--react-native',
+              label: 'React Native',
+              items: [
+                { type: 'doc', label: 'Getting started', id: 'web3modal/react-native/about' },
+                'web3modal/react-native/options',
+                'web3modal/react-native/actions',
+                'web3modal/react-native/components',
+                'web3modal/react-native/theming'
+              ]
+            },
+            {
+              type: 'category',
               label: 'Flutter',
               className: 'sidebar__logo sidebar__logo--flutter',
               items: [
