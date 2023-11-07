c6a619a699f6dacc232f701ff4e6b8fdab2543e2
Boidushya Bhattacharya <boidushyabhattacharya@gmail.com>
2023-11-07 21:47:34 +0530

    fix: changed order of nextjs

--- sidebars.js
+++ sidebars.js
@@ -30,6 +30,19 @@ module.exports = {
           collapsible: true,
           items: [
             'web3modal/about',
+			{
+			  type: 'category',
+			  className: 'sidebar__logo sidebar__logo--nextjs',
+			  label: 'Next.js',
+			  items: [
+				{ type: 'doc', label: 'Getting started', id: 'web3modal/nextjs/about' },
+				'web3modal/nextjs/hooks',
+				'web3modal/nextjs/options',
+				'web3modal/nextjs/components',
+				'web3modal/nextjs/theming',
+				'web3modal/nextjs/resources'
+			  ]
+			},
             {
               type: 'category',
               className: 'sidebar__logo sidebar__logo--react',
@@ -43,19 +56,6 @@ module.exports = {
                 'web3modal/react/resources'
               ]
 			  },
-			{
-              type: 'category',
-              className: 'sidebar__logo sidebar__logo--nextjs',
-              label: 'Next.js',
-              items: [
-                { type: 'doc', label: 'Getting started', id: 'web3modal/nextjs/about' },
-                'web3modal/nextjs/hooks',
-                'web3modal/nextjs/options',
-                'web3modal/nextjs/components',
-                'web3modal/nextjs/theming',
-                'web3modal/nextjs/resources'
-              ]
-            },
             {
               type: 'category',
               className: 'sidebar__logo sidebar__logo--vue',
