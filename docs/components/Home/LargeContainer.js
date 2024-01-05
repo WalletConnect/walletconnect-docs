4fba46efe21e04d94b0998fcc1e668f347bdc9f4
Boidushya Bhattacharya <boidushyabhattacharya@gmail.com>
2024-01-05 20:55:04 +0530

    fix: added blur to all large containers

--- docs/components/Home/LargeContainer.js
+++ docs/components/Home/LargeContainer.js
@@ -4,14 +4,10 @@ import React from 'react'
 const LargeContainer = ({ href, name, icon, description, fit, isWhite }) => {
   return (
     <Link to={href} className="home__large-container">
-      {fit ? (
-        <img src={icon} alt={name} />
-      ) : (
-        <div className="home__large-container--nofit">
-		<img src={icon} alt={name} className={isWhite ? `white` : undefined} />
-          <img className={`blur ${isWhite ? `white` : undefined}`} src={icon} alt={name} />
-        </div>
-      )}
+      <div className="home__large-container--nofit">
+        <img src={icon} alt={name} className={isWhite ? `white` : undefined} />
+        <img className={`blur ${isWhite ? `white` : undefined}`} src={icon} alt={name} />
+      </div>
 
       <div className="home__large-container--text">
         <h4>{name}</h4>
