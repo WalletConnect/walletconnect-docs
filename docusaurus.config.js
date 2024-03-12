9acf6efb6b4deff2fa43585fb8e6dbe9288a64ec
Chris Smith <1979423+chris13524@users.noreply.github.com>
2024-03-12 15:07:17 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -177,7 +177,7 @@ const config = {
     announcementBar: {
       id: 'support_us',
       content:
-        'New to Web3Modal: Email Wallets! <a rel="noopener noreferrer" href="/web3modal/features/email?utm_source=docs&utm_medium=banner">Tap to explore the latest onboarding feature</a> :zap:',
+        'New to Web3Modal: Email Wallets! <a rel="noopener noreferrer" href="/web3modal/features/email?utm_source=docs&utm_medium=banner">Tap to explore the latest onboarding feature</a> ⚡',
       backgroundColor: '#3182ce',
       textColor: '#fff',
       isCloseable: true
